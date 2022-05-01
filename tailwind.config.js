7133a80f2cf7c5b370d481e87edd81b123ec2d44
fokouarnaud <fokouarnaud@gmail.com>
2022-05-01 18:51:30 +0100

    add light theme ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,13 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      'primary':'#1E1E1E', /* 60%*/
+      'primary':'#FFFFFF', /* 60%*/
       'primary-dark':'#161616', /* 60%*/
       'primary-light':'#292C39', /* 60%*/
-      'secondary':'#252526', /* 30%*/
+      'secondary':'#292929', /* 30%*/
       'secondary-light':'#A8A8A8', /* 30%*/
       'secondary-dark':'#282828', /* 30%*/
-      'accent':'#5686E1', /* 10%*/
+      'accent':'#6186CC', /* 10%*/
       'grey-light-1':'#F2F3FC',
       'grey-light-2':'#CACBD3',
       'grey-light-3':'#8B8C95',
