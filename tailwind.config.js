b996390f206a19d1d78b5db418d325d908bb2d84
fokouarnaud <fokouarnaud@gmail.com>
2022-05-22 09:39:33 +0100

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,12 @@
 module.exports = {
   content: [
     './public/index.html',
-    './src/**/*.{html,js}'],
+    './src/**/*.{html,js}',
+  ],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'primary':'#FFFFFF', /* 60%*/
-      'primary-dark':'#161616', /* 60%*/
-      'primary-light':'#292C39', /* 60%*/
-      'secondary':'#292929', /* 30%*/
-      'secondary-light':'#A8A8A8', /* 30%*/
-      'secondary-dark':'#282828', /* 30%*/
-      'accent':'#6186CC', /* 10%*/
-      'grey-light-1':'#F2F3FC',
-      'grey-light-2':'#CACBD3',
-      'grey-light-3':'#8B8C95',
-      'grey-light-4':'#E6E7F0',
-      'grey-light-5':'#C4C6CF',
-      'grey-light-6':'#E2E2E2',
-      'grey-light-7':'#D4D4D4',
-      'grey-light-8':'#757575',
-      'grey-dark-1':'#7E7E82',
-      'grey-dark-2':'#373946',
-      'grey-dark-3':'#4D5566',
-      'grey-dark-4':'#403930',
-      'white': '#ffffff',
-      'black': '#000000',
-      'purple': '#3f3cbb',
-      'silver': '#ecebff'
-      
-    },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
+      serif: ['Lora', 'serif'],
     },
     extend: {},
   },
